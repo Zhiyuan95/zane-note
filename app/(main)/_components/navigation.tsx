f5f3aee0016d31f1cfa7252312d68f5947585a3a
Zhiyuan Zhang <68951730+Zhiyuan95@users.noreply.github.com>
2023-11-30 14:09:23 +1000

    compelte resizing navbar functionality

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,11 +1,98 @@
 "use client";
 
+import { cn } from "@/lib/utils";
+import { ChevronsLeft, MenuIcon } from "lucide-react";
+import React, { ElementRef, useRef, useState } from "react";
+import { useMediaQuery } from "usehooks-ts";
+
 const Navigation = () => {
+  const isMobile = useMediaQuery("(max-width: 768px)");
+
+  const isResizingRef = useRef(false);
+  const sidebarRef = useRef<ElementRef<"aside">>(null);
+  const navbarRef = useRef<ElementRef<"div">>(null);
+
+  const [isCollapse, setIsCollapse] = useState(isMobile);
+  const [isResetting, setIsResetting] = useState(false);
+
+  const handleMouseDown = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    isResizingRef.current = true;
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseup);
+  };
+
+  const handleMouseMove = (event: MouseEvent) => {
+    if (!isResizingRef.current) return;
+    let newWidth = event.clientX;
+
+    if (newWidth < 240) newWidth = 240;
+    if (newWidth > 480) newWidth = 480;
+
+    if (sidebarRef.current && navbarRef.current) {
+      sidebarRef.current.style.width = `${newWidth}px`;
+      navbarRef.current.style.setProperty("left", `${newWidth}px`);
+      navbarRef.current.style.setProperty(
+        "width",
+        `calc(100% - ${newWidth}px)`
+      );
+    }
+  };
+  const handleMouseup = () => {
+    isResizingRef.current = false;
+
+    document.removeEventListener("mousemove", handleMouseMove);
+    document.removeEventListener("mouseup", handleMouseup);
+  };
+
   return (
     <>
-      <aside className=" group/sidebar flex h-full bg-secondary overflow-y-auto relative w-60 flex-col z-[99999] ">
-        Navigation
+      <aside
+        ref={sidebarRef}
+        className={cn(
+          "group/sidebar flex h-full bg-secondary overflow-y-auto relative w-60 flex-col z-[99999] ",
+          isResetting && "transition-all ease-in-out duration-300",
+          isMobile && "w-0"
+        )}
+      >
+        <div
+          role="button"
+          className={cn(
+            "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
+            isMobile && "opacity-100"
+          )}
+        >
+          <ChevronsLeft className="h-6 w-6" />
+        </div>
+        <div>
+          <p>Action items</p>
+        </div>
+        <div className="mt-4">
+          <p>Documents</p>
+        </div>
+        <div
+          onMouseDown={handleMouseDown}
+          className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
+        />
       </aside>
+      <div
+        className={cn(
+          "absolute top-0 left-60 w-[cala(100%-240px",
+          isResetting && "transition-all ease-in-out",
+          isMobile && "left-0 w-full"
+        )}
+        ref={navbarRef}
+      >
+        <nav className="px-3 bg-transparent py-2 w-full">
+          {isCollapse && (
+            <MenuIcon role="button" className="h-6 to-muted-foreground w-6" />
+          )}
+        </nav>
+      </div>
     </>
   );
 };
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,6 +4,7 @@ import { Spinner } from "@/components/spinner";
 import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
+import Navigation from "./_components/navigation";
 
 const MainLayout = ({ children }: { children: ReactNode }) => {
   const { isAuthenticated, isLoading } = useConvexAuth();
@@ -18,6 +19,7 @@ const MainLayout = ({ children }: { children: ReactNode }) => {
   }
   return (
     <div className="h-full flex dark:bg-[#1f1f1f]">
+      <Navigation />
       <main className="flex-1 h-full overflow-y-auto">{children}</main>
     </div>
   );
